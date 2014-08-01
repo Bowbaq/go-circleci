0e09a71807a19cf82bfa99ead5e1e2dee700ec0a
Maxime Bury <maxime.bury@gmail.com>
2014-08-01 14:28:12 -0700

    Beef up example

--- example.go
+++ example.go
@@ -18,4 +18,32 @@ func main() {
 	for _, project := range projects {
 		fmt.Println(project.VCSURL)
 	}
+
+	recent_builds, err := circleci.RecentBuilds("", "", "")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("All builds", len(recent_builds))
+
+	alp_builds, err := circleci.RecentBuilds("Echo360", "alp", "")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("ALP builds", len(alp_builds))
+
+	master_builds, err := circleci.RecentBuilds("Echo360", "alp", "master")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Master builds", len(master_builds))
+
+	details, err := circleci.BuildDetails("Echo360", "alp", master_builds[0].BuildNum)
+
+	fmt.Println("Details", details)
 }
